1849ef3d33ebb3cd743010e51af81ad9b1516b18
davidcsussex <david.clarke@escg.ac.uk>
2021-09-26 10:00:10 +0100

    added spear

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -6,17 +6,28 @@ public class Enemy : MonoBehaviour
 {
     public GameObject player;
     public float enemySpeed;
+    private Animator anim;
+    public GameObject spear;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("player x position = " + player.transform.position.x);
+
+        //trigger enemy throw
+
+        if( Input.GetKey("t"))
+        {
+            anim.Play("throw");
+        }
+
+        //print("player x position = " + player.transform.position.x);
 
         if ( player.transform.position.x < transform.position.x )
         {
@@ -40,5 +51,33 @@ public class Enemy : MonoBehaviour
             transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
     }
+
+    void DoThrow()
+    {
+        float x, y;
+
+        x = transform.position.x;
+        y = transform.position.y+3;
+
+        GameObject newSpear = Instantiate(spear, new Vector3(x,y,0), Quaternion.identity);
+
+        Rigidbody2D rb = newSpear.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector3(-25, 3, 0);
+
+        DoFaceLeft(newSpear, true);
+    }
+
+    void DoFaceLeft(GameObject obj, bool faceLeft)
+    {
+        if (faceLeft == true)
+        {
+            obj.transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            obj.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
 
 }
---
+++ Assets/scripts/Spear.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spear : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+
+        
+        if( col.gameObject.tag == "Player")
+        {
+            rb.velocity = Vector3.zero;
+            rb.isKinematic = true;
+            
+
+
+                Object.Destroy(gameObject, 0.3f);
+
+        }
+        else
+        {
+        
+        }
+    }
+}
